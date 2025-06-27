28ab2dae00917f2a1b505821df5289ad0fab9c7b
Faithirakoze <f.irakoze2@alustudent.com>
2025-06-27 13:07:48 -0700

    Creating segments using a for loop

--- Day_20/main.py
+++ Day_20/main.py
@@ -5,15 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
+starting_positions = [(0,0), (-20,0), (-40,0)]
 
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
-
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.exitonclick()
