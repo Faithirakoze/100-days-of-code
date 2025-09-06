27fc71fdae3c3a5446c132791c3cef1e78eae8a0
Faithirakoze <f.irakoze2@alustudent.com>
2025-09-06 13:55:20 +0200

    Updated flipping cards method

--- Day_31/main.py
+++ Day_31/main.py
@@ -6,38 +6,38 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 
 data = pandas.read_csv("data/french_words.csv")
-words_dict = {row.French: row.English for (index, row) in data.iterrows()}
+to_learn = data.to_dict(orient="records")
+current_card = {}
 
 def next_card():
-    global words_dict
-    french_words_list = list(words_dict.keys())
-    random_fr_word = random.choice(french_words_list)
-    print(random_fr_word)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=random_fr_word)
 
 def flip_card():
-    english_words_list = list(words_dict.values())
-    random_en_word = random.choice(english_words_list)
-    print(random_en_word)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card['English'], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=random_en_word)
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-my_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=my_image)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-
 canvas.grid(row=0, column=0, columnspan=2)
 
 
