1e062996db387ab2684c0a5aae73c100cb65ea37
Faithirakoze <f.irakoze2@alustudent.com>
2025-09-06 14:22:01 +0200

    Flashcard app complete

--- Day_31/main.py
+++ Day_31/main.py
@@ -3,11 +3,17 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    #print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 def next_card():
     global current_card, flip_timer
@@ -23,6 +29,13 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card['English'], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -42,12 +55,12 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 
 button1_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=button1_image, highlightthickness=0, command=next_card)
-wrong_button.grid(row=1, column=0)
+unknown_button = Button(image=button1_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
 button2_image = PhotoImage(file="images/right.png")
-right_button = Button(image=button2_image, highlightthickness=0, command=next_card)
-right_button.grid(row=1, column=1)
+known_button = Button(image=button2_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
 next_card()
 
